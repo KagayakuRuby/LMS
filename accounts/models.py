86afbf053e64b9f11090e4e7af3262e6a946aebd
Ruby <mrpsk.contact@proton.me>
2025-11-09 23:30:30 +0330

    `Refactor accounts/models.py and config/settings/base.py to implement custom user model`

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,47 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser , BaseUserManager , PermissionManager
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager , PermissionsMixin
 
 
 class CustomUserManager(BaseUserManager):
-    def _create_user(self,phone,password=None,**extrafields):
+    def _create_user(self,phone,password=None,**extra_fields):
         if not phone:
             raise ValueError('The Phone field must be set')
-        user = self.model(phone=phone, **extrafields)
+        user = self.model(phone=phone, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+    
+    def create_user(self,phone,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(phone, password, **extra_fields)
+
+    def create_superuser(self,phone,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(phone, password, **extra_fields)
 
 
-# class User()
+
+
+
+class User(AbstractBaseUser , PermissionsMixin):
+    phone = models.CharField(max_length=15, unique=True ,verbose_name='شماره تلفن')
+    first_name = models.CharField(max_length=20, null=True , blank=True)
+    last_name = models.CharField(max_length=20, null=True , blank=True)
+    is_active =models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.first_name
--- config/settings/base.py
+++ config/settings/base.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES : dict = {}
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
