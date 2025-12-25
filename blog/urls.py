4306efe3b0e210f5e3e03a72f459710ae086e53c
Ruby <mrpsk.contact@proton.me>
2025-12-25 21:58:01 +0330

    some templates added and urls and views updated

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from .views import *
 from .views import *
 urlpatterns = [
     path('home/',my_view,name='home'),
-    path('article/<int:article_id>/', article_id , name='article_id')
+    path('article/<int:article_id>/', article_id , name='article_id'),
+    path('contactus/', contact_view, name='home')
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,10 @@ def my_view(request):
 
 def article_id(request,article_id):
     return HttpResponse(f"article_id: {article_id}")
+
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse(f'{name} دریافت شد.')
+    return render(request,'contactus.html')
